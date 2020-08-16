8798fda872f5d440f3b9ecbf96abbb6bb6429c87
hamedsalimzadeh1999 <hamedsalimzadeh1999@gmail.com>
2020-08-16 16:51:21 +0430

    start user app

--- core/pizaa/models.py
+++ core/pizaa/models.py
@@ -1,19 +1,8 @@
-from django.db import models
-from uuid import uuid4
-# Create your models here.
-class pizza (models.Model):
-    title = models.CharField(max_length=50)
-    info = models.TextField()
-    price = models.PositiveIntegerField()
-
-class Customer(models.Model):
-    Customerid = models.AutoField(primary_key=True)
-    PhoneNumber = models.PositiveIntegerField()
-    addres = models.CharField(max_length=50)
-class ordermodel(models.Model):
-    username = models.CharField(max_length=50)
-    phonenumber = models.PositiveIntegerField()
-    address = models.TextField(max_length=50)
-    ordertime = models.DateTimeField(auto_now_add=True)
-    ordereditems = models.CharField(max_length = 10)
+  
+from .submodel.main import *
+from .submodel.Coupon import Coupon
+from .submodel.Category import Category
+from .submodel.FoodCategory import Food
+# from .submodel.forms import fromshare
+from .submodel.Orderingitems import Order
 
